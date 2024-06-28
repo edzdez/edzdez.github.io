2b5b1554e10c87e3dc492590b8d29112db936049
Ethan Zhang <ezsland05@gmail.com>
2024-06-27 23:31:54 -0500

    gone!

--- src/routes/blog/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestEvent } from '@sveltejs/kit';
-
-export async function load(event: RequestEvent) {
-	const response = await event.fetch('/api/posts');
-	const posts = await response.json();
-	return {
-		posts
-	};
-}
--- src/routes/blog/[slug]/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestEvent } from '@sveltejs/kit';
-
-export async function load(event: RequestEvent) {
-	const { slug } = event.params;
-	const response = await event.fetch(`/api/post/${slug}`);
-	return await response.json();
-}
